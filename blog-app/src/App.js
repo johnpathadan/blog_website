a064fda29966050099740367e43e386862463c2d
johnpathadan <johnpathadan@gmail.com>
2022-08-12 13:47:58 +0530

    swiper added

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,14 +1,27 @@
-import Navbar from "./Navbar";
-import Home from "./Home";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/pagination";
+import "swiper/css";
+import './App.css';
 
+// import required modules
+import { Pagination } from "swiper";
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-      </div>
-    </div>
+    <>
+      <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+        <SwiperSlide><div><h1>Hi</h1></div></SwiperSlide>
+        <SwiperSlide><div><h1>Hi</h1></div></SwiperSlide>
+        <SwiperSlide><div><h1>Hi</h1></div></SwiperSlide>
+        <SwiperSlide><div><h1>Hi</h1></div></SwiperSlide>
+        <SwiperSlide><div><h1>Hi</h1></div></SwiperSlide>
+        <SwiperSlide><div><h1>Hi</h1></div></SwiperSlide>
+        <SwiperSlide><div><h1>Hi</h1></div></SwiperSlide>
+        <SwiperSlide><div><h1>Hi</h1></div></SwiperSlide>
+        <SwiperSlide><div><h1>Hi</h1></div></SwiperSlide>
+      </Swiper>
+    </>
   );
 }
 
